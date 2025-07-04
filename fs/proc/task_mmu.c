c1ff6135070957d8ff499eca35b30adaf49622c4
reocat <ng.ct_ml@tuta.io>
2025-07-04 21:38:34 +0300

    Fix compilation

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -384,6 +384,7 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma)
 	unsigned long start, end;
 	dev_t dev = 0;
 	const char *name = NULL;
+	struct dentry *dentry;
 
 	if (file) {
 		struct inode *inode = file_inode(vma->vm_file);
@@ -399,7 +400,7 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma)
 bypass_orig_flow:
 #endif
 		pgoff = ((loff_t)vma->vm_pgoff) << PAGE_SHIFT;
-		struct dentry *dentry = file->f_path.dentry;
+		dentry = file->f_path.dentry;
         if (dentry) {
         	const char *path = (const char *)dentry->d_name.name; 
             	if (strstr(path, "lineage") ||
